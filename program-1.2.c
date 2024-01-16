8a6cf5342cde9a8113cdbc71d17c2d13fbc97ed1
Harsh-Mangukiya <mangukiyah111@gmail.com>
2024-01-16 17:16:26 +0530

    c-program-1

---
+++ program-1.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main() {
+
+	printf("\n\n\n\n");
+
+	printf("\t     *\n");
+	printf("\t    *** \n");
+	printf("\t   *****  \n");
+	printf("\t  ******* \n");
+	printf("\t *********\n");
+	printf("\t***********\n");
+
+
+}
